64954d08b42a831f5a75d780d76ce005e9ecba69
NikiKerezov <72574892+NikiKerezov@users.noreply.github.com>
2020-11-06 11:15:14 +0200

    Add files via upload

--- domashno_1.3.py
+++ domashno_1.3.py
@@ -5,6 +5,6 @@ a.sort()
 print(a)
 for i in range(len(a)):
     print(i)
-    if str[i] != b:
-        print(''', str[i], ''', 'appears', a.count(str[i]), 'times in this string')
-    b = a[i]
+    if str[i] != str[i-1]:
+        print(''', str[i], ''', 'appears', str.count(str[i]), 'times in this string')
+    
